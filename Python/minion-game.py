fc335facda5e2148b2612b7ac73f2b97e6e7e5e4
vladbortnik <vladbortnik@icloud.com>
2020-07-31 21:05:34 -0400

    minion-game

---
+++ Python/minion-game.py
@@ -0,0 +1,15 @@
+#
+# all_word = [letter_list[start:end+1] for start in range(l)
+#                                      for end in range(start, l)]
+# for array in all_word:
+#     if array[0] in vowel:
+#         person_b_words.append(array)
+# for array in all_word:
+#     if array[0] in consonants:
+#         person_a_words.append(array)
+# if len(person_a_words) == len(person_b_words):
+#     print 'Draw'
+# if len(person_a_words) > len(person_b_words):
+#     print person_a_name, len(person_a_words)
+# if len(person_b_words) > len(person_a_words):
+#     print person_b_name, len(person_b_words)
